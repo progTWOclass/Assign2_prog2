3e15316854b7c2262e22be6bc47737aabd9ac32f
SteveBanh02 <steve.banh02@gmail.com>
2025-03-15 01:35:57 -0400

    Added comments for InsufficientFundsException

--- src/InsufficientFundsException.java
+++ src/InsufficientFundsException.java
@@ -3,6 +3,10 @@
 // Written by: Steve Banh 1971537
 // For “Programming 2” Section 2 – Winter 2025
 // --------------------------------------------------------
+/**
+*This class is a custom exception that is in charge of handling 
+*problems with the customer's overdraft limit
+**/
 public class InsufficientFundsException extends Exception {
 
     public InsufficientFundsException() {
